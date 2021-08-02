e9fad581b4657a2eaa00e6a76db568b229ea6c87
JHNC99 <jonathanhnc75@gmail.com>
2021-08-01 23:37:30 -0500

    Opteniedo api y verficando loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tours from "./components/Tours";
-
+import Loading from "./components/Loading";
 function App() {
+  const url=`https://course-api.com/react-tours-project`
+  const [loading,setLoading]=useState(true);
+  const[tours,setTours]=useState([]);
+  
+  //Get Api date
+  const getTours=async ()=>{
+   
+    try{
+      const response=await fetch(url);
+      const tours= await response.json();
+      setLoading(false);
+      setTours(tours);
+    }
+    catch(error){
+      setLoading(true);
+      console.log(error);
+    }
+
+  }
+  //Changue Api
+  useEffect(()=>{
+    getTours();
+  },[])
+ 
+  if(loading){
+    return(
+      <Loading/>
+    )
+  }
   return (
     <div className="container">
       <h2 className="text-center">Tours</h2>
